7b30d5166a3ca008a1f55ef3ea29c3257e6f7b6f
frankrobles360 <60746266+frankrobles360@users.noreply.github.com>
2020-05-01 12:09:18 -0500

    Create emfancygallery.js

---
+++ emfancygallery.js
@@ -0,0 +1,86 @@
+// TAB2
+function go2tab2() {
+  var element = document.getElementById("collapse2");
+
+  if (element.classList) { 
+    element.classList.toggle("collapse");
+  } else {
+    var classes = element.className.split(" ");
+    var i = classes.indexOf("panel-collapse collapse");
+
+    if (i >= 0) 
+      classes.splice(i, 1);
+    else 
+      classes.push("panel-collapse collapse");
+      element.className = classes.join(" "); 
+  }
+}
+// TAB3
+function go2tab3() {
+  var element = document.getElementById("collapse3");
+
+  if (element.classList) { 
+    element.classList.toggle("collapse");
+  } else {
+    var classes = element.className.split(" ");
+    var i = classes.indexOf("panel-collapse collapse");
+
+    if (i >= 0) 
+      classes.splice(i, 1);
+    else 
+      classes.push("panel-collapse collapse");
+      element.className = classes.join(" "); 
+  }
+}
+// TAB4
+function go2tab4() {
+  var element = document.getElementById("collapse4");
+
+  if (element.classList) { 
+    element.classList.toggle("collapse");
+  } else {
+    var classes = element.className.split(" ");
+    var i = classes.indexOf("panel-collapse collapse");
+
+    if (i >= 0) 
+      classes.splice(i, 1);
+    else 
+      classes.push("panel-collapse collapse");
+      element.className = classes.join(" "); 
+  }
+}
+
+// TAB5
+function go2tab5() {
+  var element = document.getElementById("collapse5");
+
+  if (element.classList) { 
+    element.classList.toggle("collapse");
+  } else {
+    var classes = element.className.split(" ");
+    var i = classes.indexOf("panel-collapse collapse");
+
+    if (i >= 0) 
+      classes.splice(i, 1);
+    else 
+      classes.push("panel-collapse collapse");
+      element.className = classes.join(" "); 
+  }
+}
+// TAB6
+function go2tab6() {
+  var element = document.getElementById("collapse6");
+
+  if (element.classList) { 
+    element.classList.toggle("collapse");
+  } else {
+    var classes = element.className.split(" ");
+    var i = classes.indexOf("panel-collapse collapse");
+
+    if (i >= 0) 
+      classes.splice(i, 1);
+    else 
+      classes.push("panel-collapse collapse");
+      element.className = classes.join(" "); 
+  }
+}
